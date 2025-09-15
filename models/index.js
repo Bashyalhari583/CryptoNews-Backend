3d4fae4d0f4afd5bcc5aa98b73b9ebb0949eff6f
Bashyalhari583 <hbashyal878@gmail.com>
2025-09-15 13:26:14 +0545

    modify models/index.js

--- models/index.js
+++ models/index.js
@@ -40,6 +40,5 @@ Object.keys(db).forEach(modelName => {
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-db.Event = require('./Event')(sequelize, Sequelize.DataTypes);
 
 module.exports = db;
