822ab602a6987835c9a760327f3fdb8240fbcac5
Bashyalhari583 <hbashyal878@gmail.com>
2025-09-15 14:18:26 +0545

    config manage

--- models/index.js
+++ models/index.js
@@ -6,7 +6,7 @@ const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
 let sequelize;
@@ -40,5 +40,5 @@ Object.keys(db).forEach(modelName => {
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-
+db.Event = require('./event')(sequelize, Sequelize);
 module.exports = db;
